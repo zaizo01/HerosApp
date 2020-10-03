1b5429445b8875206a9cd2e307ebbfd4f7861974
Cristopher Zaiz <e.zaizortega@gmail.com>
2020-10-02 22:07:15 -0400

    animation of components

--- src/components/Heros/HerosList.js
+++ src/components/Heros/HerosList.js
@@ -1,13 +1,14 @@
 import React from 'react'
+import { useMemo } from 'react';
 import { getHeroByPublisher } from '../../selectors/getHeroByPublisher'
 import { HeroCard } from './HeroCard';
 
 export const HerosList = ({ publisher }) => {
 
-    const heros = getHeroByPublisher( publisher );
+    const heros = useMemo(() => getHeroByPublisher( publisher ), [ publisher ]);
 
     return (
-            <div className="flex flex-wrap justify-center">
+            <div className="flex flex-wrap justify-center animate__animated animate__fadeIn">
                 {
                     heros.map( hero => (
                         <HeroCard key={ hero.id }
--- src/components/Heros/HerosPage.js
+++ src/components/Heros/HerosPage.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useMemo } from 'react'
 import { Redirect, useParams } from 'react-router-dom'
 import { getHeroByid } from '../../selectors/getHeroByid';
 
@@ -6,7 +6,7 @@ export const HerosPage = ({ history }) => {
     
     const { heroeId } = useParams();
 
-    const hero = getHeroByid( heroeId );
+    const hero = useMemo(() => getHeroByid( heroeId ), [ heroeId ])
     
     if( !hero ) {
         return <Redirect to='/' />
@@ -28,9 +28,9 @@ export const HerosPage = ({ history }) => {
     const { superhero, publisher, alter_ego, first_appearance, characters } = hero;
 
     return (
-        <div className="flex shadow-lg bg-white rounded-lg overflow-hidden w-4/5">
+        <div className="flex w-4/5 overflow-hidden bg-white rounded-lg shadow-lg">
             <div className="w-1/4 bg-cover">
-            <img src={ `../assets/heroes/${ heroeId }.jpg` } alt={ superhero }/>
+            <img src={ `../assets/heroes/${ heroeId }.jpg` } alt={ superhero } className="animate__animated animate__fadeInLeft"/>
             </div>
             <div className="w-1/2 p-6 my-auto">
                 <h3 className="text-4xl font-semibold">{ superhero }</h3>
@@ -40,7 +40,7 @@ export const HerosPage = ({ history }) => {
                     <li><p className="font-semibold">Frist appearance:</p> { first_appearance }</li>
                     <li><p className="font-semibold">Characters:</p> { characters }</li>
                 </ul>
-                <button className="bg-black hover:bg-gray-800 text-white font-semibold rounded-md py-4 px-16 mt-5"
+                <button className="px-16 py-4 mt-5 font-semibold text-white bg-black rounded-md hover:bg-gray-800"
                         onClick={ handleReturn }
                         >Back</button>
             </div>
